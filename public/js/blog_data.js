152440e05c3ac2ed1e1388918771c4b1cb1d3dcb
Paul Love <paullove@Pauls-MacBook-Pro.local>
2014-12-19 20:22:55 -0600

    random

---
+++ public/js/blog_data.js
@@ -0,0 +1,33 @@
+var data =  [
+	{
+		"title": "Blog Title 1",
+		"date": "2014-12-18",
+		"author": "Paul Love",
+		"content": "Lorem ipsum mustache oil,velit fist horse power motor oil sit ultrices primis boots Harley Davidson, fist sagittis boots imperdiet chopper praesent porttitor motor oil sledge hammer, horse power fames himenaeos fist himenaeos ultricies sledge hammer aptent ipsum. Fringilla boots fermentum porta sledge hammer quis motor oil nullam Harley Davidson fist chopper My little Pony melts my heart"
+	},
+	{
+		"title": "Blog Title 2",
+		"date": "2014-12-1",
+		"author": "Paul Love",
+		"content": "MEow Meow meow meo,velit fist horse power motor oil sit ultrices primis boots Harley Davidson, fist sagittis boots imperdiet chopper praesent porttitor motor oil sledge hammer, horse power fames himenaeos fist himenaeos ultricies sledge hammer aptent ipsum. Fringilla boots fermentum porta sledge hammer quis motor oil nullam Harley Davidson fist chopper My little Pony melts my heart"
+	},
+	{
+		"title": "Blog Title 3",
+		"date": "2014-11-18",
+		"author": "Paul Love",
+		"content": "volmustache oil,velit taco fist is banned horse power motor oil sit ultrices primis boots Harley Davidson, fist sagittis boots imperdiet chopper praesent porttitor motor oil sledge hammer, horse power fames himenaeos fist himenaeos ultricies sledge hammer aptent ipsum. Fringilla boots fermentum porta sledge hammer quis motor oil nullam Harley Davidson fist chopper My little Pony melts my heart"
+	},
+	{
+		"title": "Blog Title 4",
+		"date": "2014-10-18",
+		"author": "Paul Love",
+		"content": "I didn't doo it rhonda se power motor oil sit ultrices primis boots Harley Davidson, fist sagittis boots imperdiet chopper praesent porttitor motor oil sledge hammer, horse power fames himenaeos fist himenaeos ultricies sledge hammer aptent ipsum. Fringilla boots fermentum porta sledge hammer quis motor oil nullam Harley Davidson fist chopper My little Pony melts my heart"
+	},
+	{
+		"title": "Blog Title 5",
+		"date": "2014-2-18",
+		"author": "Paul Love",
+		"content": "Make it stop make it stop ..ohow ipsumhorse power motor oil sit ultrices primis boots Harley Davidson, fist sagittis boots imperdiet chopper praesent porttitor motor oil sledge hammer, horse power fames himenaeos fist himenaeos ultricies sledge hammer aptent ipsum. Fringilla boots fermentum porta sledge hammer quis motor oil nullam Harley Davidson fist chopper My little Pony melts my heart"
+	}
+];
+
