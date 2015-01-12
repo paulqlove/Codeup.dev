f196f297bf59787c866f7a2ed273774c32cdb4c7
Paul Love <paullove@Pauls-MacBook-Pro.local>
2015-01-12 09:54:00 -0600

    you are getting lazy

--- public/acme/includes/nav.php
+++ public/acme/includes/nav.php
@@ -1,13 +1,13 @@
 <nav class="navbar navbar-default navbar-fixed-top">
   <div class="container">
-<h1>Nav</h1>
-	<ul>
-		<li>Home</li>
-		<li>About</li>
-		<li>Contact</li>
-		<li>Locations</li>
-	</ul>
-	
-   
-  </div>
+	<h1>Nav</h1>
+		<ul>
+			<li>Home</li>
+			<li>About</li>
+			<li>Contact</li>
+			<li>Locations</li>
+		</ul>
+		
+	   
+	  </div>
 </nav>
